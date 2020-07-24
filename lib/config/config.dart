db32a9d3edafc94791121d38f2556dacde90faa6
Patrick O'Hara <pohara60@gmail.com>
2020-07-24 15:55:19 +0100

    Document Server IP address

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,1 +1,4 @@
+// Server IP address and port
+//! localhost does not work with Android emulator which uses 10.0.0.2
+//! To support both Web and Android emulator use the (local) IP address of the server
 const BASE_URL = 'http://192.168.1.159:5000';
